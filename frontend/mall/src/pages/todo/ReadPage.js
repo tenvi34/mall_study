dd425c71d6bf0fae4a6b76f873441fd519be1320
JWT <ryuhoken30@gamil.com>
2024-03-13 22:16:48 +0900

    Todo에서 ReadPage 생성

---
+++ frontend/mall/src/pages/todo/ReadPage.js
@@ -0,0 +1,11 @@
+
+
+const ReadPage = () => {
+    return (
+        <div className="text-3xl font-extrabold">
+            Todo Read Page Component
+        </div>
+    );
+}
+
+export default ReadPage;
--- frontend/mall/src/router/todoRouter.js
+++ frontend/mall/src/router/todoRouter.js
@@ -3,6 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const Loading = <div>Loading....</div>
 const TodoList = lazy(() => import("../pages/todo/ListPage"));
+const ReadPage = lazy(() => import("../pages/todo/ReadPage"));
 
 const todoRouter = () => {
     return [
@@ -12,7 +13,11 @@ const todoRouter = () => {
         },
         {
             path: "",
-            element: <Navigate replace to="list"/>
+            element: <Navigate replace to="/todo/list"/>
+        },
+        {
+            path: "read/:tno",
+            element: <Suspense fallback={Loading}><ReadPage/></Suspense>
         }
     ]
 }
