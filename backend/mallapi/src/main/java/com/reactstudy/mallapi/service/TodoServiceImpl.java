6f3b93736fbcce9b9740cd066b71492e24104784
JWT <ryuhoken30@gamil.com>
2024-03-15 01:31:55 +0900

    Revert "조회 기능 구현"
    This reverts commit 8e82b1b7b3092cce379459f54e696184589c48d2.

--- backend/mallapi/src/main/java/com/reactstudy/mallapi/service/TodoServiceImpl.java
+++ backend/mallapi/src/main/java/com/reactstudy/mallapi/service/TodoServiceImpl.java
@@ -1,35 +1,22 @@
 package com.reactstudy.mallapi.service;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.reactstudy.mallapi.domain.Todo;
 import com.reactstudy.mallapi.dto.TodoDTO;
-import com.reactstudy.mallapi.repository.TodoRepository;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Transactional
 @Log4j2
-@RequiredArgsConstructor
-public class TodoServiceImpl implements TodoService {
-
-    private final ModelMapper modelMapper;
-
-    private final TodoRepository todoRepository;
+public class TodoServiceImpl implements TodoService{
     
     @Override
     public Long register(TodoDTO todoDTO) {
 
         log.info("...............");
 
-        Todo todo = modelMapper.map(todoDTO, Todo.class);
-
-        Todo savedTodo = todoRepository.save(todo);
-
-        return savedTodo.getTno();
+        return null;
     }
 }
