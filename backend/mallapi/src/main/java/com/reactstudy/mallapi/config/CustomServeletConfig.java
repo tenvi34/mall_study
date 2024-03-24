e96fc44a7f1abd6f38a4cdb954f8d05a220bf110
JWT <ryuhoken30@gamil.com>
2024-03-24 21:23:18 +0900

    CORS 설정

--- backend/mallapi/src/main/java/com/reactstudy/mallapi/config/CustomServeletConfig.java
+++ backend/mallapi/src/main/java/com/reactstudy/mallapi/config/CustomServeletConfig.java
@@ -2,6 +2,7 @@ package com.reactstudy.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.reactstudy.mallapi.controller.formatter.LocalDateFormatter;
@@ -12,5 +13,16 @@ public class CustomServeletConfig implements WebMvcConfigurer{
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    // CORS 설정(교차 출처 리소스 공유)
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 }
