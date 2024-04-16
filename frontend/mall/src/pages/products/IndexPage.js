09b3027fdcf344f75ee422981777d9aadf8c73db
JWT <ryuhoken30@gamil.com>
2024-04-17 00:56:14 +0900

    list 링크 연결

--- frontend/mall/src/pages/products/IndexPage.js
+++ frontend/mall/src/pages/products/IndexPage.js
@@ -1,7 +1,41 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import BasicLayout from "../../layout/BasicLayout";
+import { useCallback } from "react";
+
 const IndexPage = () => {
+
+    const navigate = useNavigate()
+
+    const handleClickList = useCallback(() => {
+        navigate({ pathname: 'list' })
+    })
+
+    const handleClickAdd = useCallback(() => {
+        navigate({ pathname: 'add' })
+    })
+
     return (
-        <></>
-    )
+        <BasicLayout>
+
+            <div className="text-black font-extrabold -mt-10">
+                Products Menu
+            </div>
+
+            {/* LIST */}
+            <div className="w-full flex m-2 p-2">
+                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline" onClick={handleClickList}>LIST</div>
+            </div>
+            {/* ADD */}
+            <div className="w-full flex m-2 p-2">
+                <div className="text-xl m-1 p-2 w-20 font-extrabold text-center underline" onClick={handleClickAdd}>ADD</div>
+            </div>
+
+            <div className="flex flex-wrap w-full">
+                <Outlet />
+            </div>
+
+        </BasicLayout>
+    );
 }
 
 export default IndexPage;
--- frontend/mall/src/router/productsRouter.js
+++ frontend/mall/src/router/productsRouter.js
@@ -1,6 +1,20 @@
+import { Suspense, lazy } from "react";
+import { Navigate } from "react-router-dom";
+
 const productsRouter = () => {
+
+    const Loading = <div>Loading....</div>
+    const ProductsList = lazy(() => import("../pages/products/ListPage"))
+
     return [
-        
+        {
+            path: "list",
+            element: <Suspense fallback={Loading}><ProductsList/></Suspense>
+        },
+        {
+            path: "",
+            element: <Navigate replace to="/products/list"/>
+        },
     ]
 }
 
