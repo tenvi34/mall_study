4ffc91ac0d522149c7a5e479023db5458880381e
JWT <ryuhoken30@gamil.com>
2024-04-19 16:46:59 +0900

    상품 목록 이미지 보이기

--- frontend/mall/src/api/productsApi.js
+++ frontend/mall/src/api/productsApi.js
@@ -5,9 +5,18 @@ const host = `${API_SERVER_HOST}/api/products`
 
 export const postAdd = async (product) => {
 
-    const header = {headers: {"Content-Type" : "multipart/form-data"}}
+    const header = { headers: { "Content-Type": "multipart/form-data" } }
 
     const res = await axios.post(`${host}/`, product, header)
+
+    return res.data
+}
+
+export const getList = async (pageParam) => {
+
+    const { page, size } = pageParam
+
+    const res = await axios.get(`${host}/list`, { params: { page: page, size: size } })
 
     return res.data
 }
--- frontend/mall/src/pages/products/ListPage.js
+++ frontend/mall/src/pages/products/ListPage.js
@@ -1,9 +1,14 @@
+import ListComponent from "../../components/products/ListComponent";
+
 const ListPage = () => {
     return (
         <div className="p-4 w-full bg-white">
             <div className="text-3xl font-extrabold">
                 Products List Page
             </div>
+
+            <ListComponent/>
+            
         </div>
     );
 }
