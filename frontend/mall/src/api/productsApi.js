10fe9d29adeccd66371ddaff2c575b8cf72df566
JWT <ryuhoken30@gamil.com>
2024-04-20 22:04:22 +0900

    수정/삭제/목록으로 추가 완료

--- frontend/mall/src/api/productsApi.js
+++ frontend/mall/src/api/productsApi.js
@@ -3,6 +3,7 @@ import { API_SERVER_HOST } from "./todoApi"
 
 const host = `${API_SERVER_HOST}/api/products`
 
+// 상품 추가
 export const postAdd = async (product) => {
 
     const header = { headers: { "Content-Type": "multipart/form-data" } }
@@ -12,6 +13,7 @@ export const postAdd = async (product) => {
     return res.data
 }
 
+// 목록 출력
 export const getList = async (pageParam) => {
 
     const { page, size } = pageParam
@@ -21,9 +23,28 @@ export const getList = async (pageParam) => {
     return res.data
 }
 
+// 특정 상품 조회
 export const getOne = async (pno) => {
 
     const res = await axios.get(`${host}/${pno}`)
+
+    return res.data
+}
+
+// 수정
+export const putOne = async (pno, product) => {
+
+    const header = { headers: { "Content-Type": "multipart/form-data" } }
+
+    const res = await axios.put(`${host}/${pno}`, product, header)
+
+    return res.data
+}
+
+// 삭제
+export const deleteOne = async (pno) => {
+
+    const res = await axios.delete(`${host}/${pno}`)
 
     return res.data
 }
--- frontend/mall/src/router/productsRouter.js
+++ frontend/mall/src/router/productsRouter.js
@@ -8,6 +8,7 @@ const productsRouter = () => {
     const ProductsList = lazy(() => import("../pages/products/ListPage"))
     const ProductsAdd = lazy(() => import("../pages/products/AddPage"))
     const ProductsRead = lazy(() => import("../pages/products/ReadPage"))
+    const ProductsModify = lazy(() => import("../pages/products/ModifyPage"))
 
     return [
         {
@@ -25,6 +26,10 @@ const productsRouter = () => {
         {
             path: "read/:pno",
             element: <Suspense fallback={Loading}><ProductsRead/></Suspense>
+        },
+        {
+            path: "modify/:pno",
+            element: <Suspense fallback={Loading}><ProductsModify/></Suspense>
         }
     ]
 }
